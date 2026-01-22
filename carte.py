a441f27a7cd5f145189d4cf0e2310e7d48c651d6
Veldt Cocktail <156076403+VeldtCocktail@users.noreply.github.com>
2026-01-22 16:32:52 +0100

    Il manquait le data/

--- carte.py
+++ carte.py
@@ -2,6 +2,6 @@ import folium
 
 carte = folium.Map(location=(46.3930189, -1.480289), zoom_start=12)
 
-folium.GeoJson("forets_vendee.geojson").add_to(carte)
+folium.GeoJson("data/forets_vendee.geojson").add_to(carte)
 
 carte.save("carte.html")
