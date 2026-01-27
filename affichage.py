7491fb540b1c1c16b404d95ea378091ffbdd6641
unknown <mathjo85@gmail.com>
2026-01-27 09:10:52 +0100

    affichage

--- affichage.py
+++ affichage.py
@@ -1,26 +1,50 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout
+)
 from PyQt5 import QtWebEngineWidgets
 from pathlib import Path
 import sys
 import os
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Affichage de carte")
+        self.setWindowTitle("Affichage carte des forêts")
         
+        layout_principal = QHBoxLayout()
+        layout_boutons = QVBoxLayout()
+
         view = QtWebEngineWidgets.QWebEngineView()
         # besoin d'être connecté à Internet pour que la carte folium marche
         html = Path("cartes", "carte.html").read_text(encoding="utf8")
         view.setHtml(html)
-        self.setCentralWidget(view)
+        
+        bouton_ajouter_foret = QPushButton("Ajouter forêt")
+        bouton_ajouter_foret.show()
 
+        bouton_supprimer_foret = QPushButton("Supprimer forêt")
+        bouton_supprimer_foret.show()
+        bouton_supprimer_foret.clicked.connect(self.appui_bouton)
+
+        layout_boutons.addWidget(bouton_ajouter_foret)
+        layout_boutons.addWidget(bouton_supprimer_foret)
+
+        layout_principal.addLayout(layout_boutons)
+        layout_principal.addWidget(view)
+
+        self.setLayout(layout_principal)
+        self.show()
+
+    def appui_bouton(self):
+        self.close()
+    
 app = QApplication(sys.argv)
-
-
 window = MainWindow()
-window.show()
 
 
 app.exec()
