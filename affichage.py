a59ef31a5622f419e1548a31885b2b90cf33d85d
unknown <mathjo85@gmail.com>
2026-01-29 17:40:27 +0100

    affichage

--- affichage.py
+++ affichage.py
@@ -11,13 +11,50 @@ from pathlib import Path
 import sys
 import os
 
+class ForestWindow(QWidget):
+    def __init__(self, parent = None):
+        super().__init__()
+        self.parent = parent
+
+        layout_principal = QHBoxLayout
+        layout = QVBoxLayout()
+        layout_texte = QVBoxLayout()
+
+        self.setLayout(layout_principal)
+
+        self.donnee1 = QLineEdit("")
+        self.donnee2 = QLineEdit("")
+        self.donnee3 = QLineEdit("")
+        self.donnee4 = QLineEdit("")
+        self.donnee5 = QLineEdit("")
+        self.donnee6 = QLineEdit("")
+        self.donnee7 = QLineEdit("")
+
+        layout_principal.addLayout(layout_texte)
+        layout_principal.addLayout(layout)
+
+        layout.addWidget(self.donnee1)
+        layout.addWidget(self.donnee2)
+        layout.addWidget(self.donnee3)
+        layout.addWidget(self.donnee4)
+        layout.addWidget(self.donnee5)
+        layout.addWidget(self.donnee6)
+        layout.addWidget(self.donnee7)
+
+        self.hide()
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.fenetre_forets = ForestWindow(self)
+
         self.setWindowTitle("Affichage carte des forêts")
         
         layout_principal = QHBoxLayout()
+
+        layout_principal.addWidget(self.fenetre_forets)
+
         layout_boutons = QVBoxLayout()
 
         view = QtWebEngineWidgets.QWebEngineView()
@@ -26,15 +63,15 @@ class MainWindow(QWidget):
         view.setHtml(html)
         
         self.champ = QLineEdit("")
-        self.champ.show()
 
         ### BOUTON
         bouton_ajouter_foret = QPushButton("Ajouter forêt")
         bouton_ajouter_foret.show()
+        bouton_ajouter_foret.clicked.connect(self.creation_foret_fenetre)
+
 
         bouton_supprimer_foret = QPushButton("Supprimer forêt")
         bouton_supprimer_foret.show()
-        bouton_supprimer_foret.clicked.connect(self.appui_bouton)
 
         ### GESTION LAYOUT
         layout_boutons.addWidget(self.champ)
@@ -48,13 +85,11 @@ class MainWindow(QWidget):
         self.setLayout(layout_principal)
         self.show()
 
-    def appui_bouton(self):
-        texte = self.champ.text()
-        print(texte)
+    def creation_foret_fenetre(self):
+        self.fenetre_forets.show()
     
     
 app = QApplication(sys.argv)
 window = MainWindow()
-
 
 app.exec()
