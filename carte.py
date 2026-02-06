3d56f106e47e4e64cf1fcfdddc888ca7b273cb0e
LambdaLight4205 <lambda.light@proton.me>
2026-02-06 11:23:46 +0100

    carte

--- carte.py
+++ carte.py
@@ -1,39 +1,50 @@
 import folium
 import pathlib
 
-carte = folium.Map(location=(46.3930189, -1.480289), zoom_start=12)
+def generer_carte(coordonnees_depart, donnees = []):
+    fonction_style = lambda x: {
+        "fillColor": (
+            "#0000ff"
+        )
+    }
 
-folium.GeoJson("data/forets_vendee.geojson").add_to(carte)
+    carte = folium.Map(location = coordonnees_depart, zoom_start = 12)
 
-map_name = carte.get_name()
+    folium.GeoJson("data/forets_vendee.geojson").add_to(carte)
+    for donnees_json in donnees:
+        folium.GeoJson(donnees_json, style_function = fonction_style)
 
+    map_name = carte.get_name()
 
-# portion de code générée par l'intelligence artificielle
-click_js = f"""
-function bindMapClick() {{
-    if (typeof {map_name} !== 'undefined') {{
-        {map_name}.on('click', function(e) {{
-            if (window.pybridge) {{
-                pybridge.envoyerCoordonnees(e.latlng.lat, e.latlng.lng);
-            }}
-        }});
-    }} else {{
-        setTimeout(bindMapClick, 100);
+    # portion de code générée par l'intelligence artificielle
+    click_js = f"""
+    function bindMapClick() {{
+        if (typeof {map_name} !== 'undefined') {{
+            {map_name}.on('click', function(e) {{
+                if (window.pybridge) {{
+                    pybridge.envoyerCoordonnees(e.latlng.lat, e.latlng.lng);
+                }}
+            }});
+        }} else {{
+            setTimeout(bindMapClick, 100);
+        }}
     }}
-}}
-bindMapClick();
-"""
+    bindMapClick();
+    """
 
-qwebchannel_js = """
-<script src="qrc:///qtwebchannel/qwebchannel.js"></script>
-<script>
-new QWebChannel(qt.webChannelTransport, function(channel) {
-    window.pybridge = channel.objects.pybridge;
-});
-</script>
-"""
+    qwebchannel_js = """
+    <script src="qrc:///qtwebchannel/qwebchannel.js"></script>
+    <script>
+    new QWebChannel(qt.webChannelTransport, function(channel) {
+        window.pybridge = channel.objects.pybridge;
+    });
+    </script>
+    """
 
-carte.get_root().script.add_child(folium.Element(click_js))
-carte.get_root().html.add_child(folium.Element(qwebchannel_js))
+    carte.get_root().script.add_child(folium.Element(click_js))
+    carte.get_root().html.add_child(folium.Element(qwebchannel_js))
 
-carte.save(pathlib.Path("cartes", "carte.html"))
+    carte.save(pathlib.Path("cartes", "carte.html"))
+
+
+generer_carte((46.3930189, -1.480289))
--- gestion_clicks.py
+++ gestion_clicks.py
@@ -14,15 +14,55 @@ from PyQt5.QtCore import QUrl
 from pathlib import Path
 import sys
 import os
+import overpass
+from carte import generer_carte
+
+class RequetesOverpass:
+    def __init__(self):
+        self.api = overpass.API(timeout = 60)
+
+    def zone_verte(self, coords):
+        lat, lon = coords
+
+        requete = f"""
+        [out:json][timeout:25];
+        (
+        way["landuse"="forest"](around:0,{lat},{lon});
+        way["natural"="wood"](around:0,{lat},{lon});
+        way["landcover"="trees"](around:0,{lat},{lon});
+        way["natural"="scrub"](around:0,{lat},{lon});
+        way["landuse"="orchard"](around:0,{lat},{lon});
+
+        relation["landuse"="forest"](around:0,{lat},{lon});
+        relation["natural"="wood"](around:0,{lat},{lon});
+        relation["landcover"="trees"](around:0,{lat},{lon});
+        relation["natural"="scrub"](around:0,{lat},{lon});
+        relation["landuse"="orchard"](around:0,{lat},{lon});
+        );
+        out body;
+        >;
+        out skel qt;
+        """
+
+        reponse = self.api.get(requete, responseformat='json')
+
+        return reponse
+
 
 class Pont(QObject):
+
+    def __init__(self, parent):
+        self.par = parent
 
     @pyqtSlot(float, float)
     def envoyerCoordonnees(self, lat, long):
         print(f'Click enregistré en : {lat}, {long}')
+        self.par.update_carte((lat, long))
 
 class MainWindow(QWidget):
     def __init__(self):
+        self.requetes = RequetesOverpass()
+
         super().__init__()
 
         self.setWindowTitle("Affichage carte des forêts")
@@ -30,19 +70,19 @@ class MainWindow(QWidget):
         layout_principal = QHBoxLayout()
         layout_boutons = QVBoxLayout()
 
-        view = QtWebEngineWidgets.QWebEngineView()
+        self.view = QtWebEngineWidgets.QWebEngineView()
         # besoin d'être connecté à Internet pour que la carte folium marche
         html_path = Path("cartes", "carte.html").resolve()
 
-        view.setHtml(
+        self.view.setHtml(
             html_path.read_text(encoding="utf8"),
             QUrl.fromLocalFile(str(html_path.parent) + "/")
         )
         
-        self.bridge = Pont()
+        self.bridge = Pont(self)
         self.channel = QWebChannel()
         self.channel.registerObject("pybridge", self.bridge)
-        view.page().setWebChannel(self.channel)
+        self.view.page().setWebChannel(self.channel)
 
 
         self.champ = QLineEdit("")
@@ -62,7 +102,7 @@ class MainWindow(QWidget):
         layout_boutons.addWidget(bouton_supprimer_foret)
 
         layout_principal.addLayout(layout_boutons)
-        layout_principal.addWidget(view)
+        layout_principal.addWidget(self.view)
         
 
         self.setLayout(layout_principal)
@@ -71,6 +111,11 @@ class MainWindow(QWidget):
     def appui_bouton(self):
         texte = self.champ.text()
         print(texte)
+
+    def update_carte(self, coords):
+        zone = self.requetes.zone_verte(coords)
+        generer_carte(coords, [zone])
+        self.view.update()
     
     
 app = QApplication(sys.argv)
