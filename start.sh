087558db02e5e0c0b5136ac7c68802cff517db97
LambdaLight4205 <lambda.light@proton.me>
2026-03-03 09:33:55 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,12 @@ sudo apt install \
     libxcb-image0 \
     libxcb-keysyms1 \
     libxcb-randr0 \
+    qtwayland5 \
+    python3-pyqt5.qtwebengine \
     libxcb-render-util0 \
     libxcb-shape0 \
     libxcb-xinerama0 \
     libxcb-xkb1 \
     libxkbcommon-x11-0 \
-    libgl1-mesa-glx \
     libegl1
-QT_QPA_PLATFORM=wayland python3 sources/main.py
+QT_QPA_PLATFORM=xcb QT_DEBUG_PLUGINS=1 python3 sources/main.py
