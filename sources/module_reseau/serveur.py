4b84166fea88994f159dda5e58b4e78c41b49d2e
VeldtCocktail <obamiumdidier@gmail.com>
2026-03-19 15:48:33 +0100

    fix d'une erreur de bind d'ip

--- sources/module_reseau/serveur.py
+++ sources/module_reseau/serveur.py
@@ -1,15 +1,9 @@
 import http.server
 import socketserver
 
-
 def lancer_serveur():
     handler = http.server.SimpleHTTPRequestHandler
+    socketserver.TCPServer.allow_reuse_address = True
     with socketserver.TCPServer(("127.0.0.1", 8000), handler) as httpd:
-        print("Serveur ouvert")
+        print("Serveur ouvert sur http://127.0.0.1:8000")
         httpd.serve_forever()
-        
-def fermer_seveur():
-    handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("127.0.0.1", 8000), handler) as httpd:
-        print("Serveur fermé")
-        httpd.shutdown()
